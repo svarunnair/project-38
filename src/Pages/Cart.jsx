fa8a57b3cf6ffedd04ab91c5f20aca53c582c78e
Varun Nair <varunsnair1997@gmail.com>
2024-01-26 22:46:01 +0530

    cart page

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography, styled } from '@mui/material';
+import { Box, Button, Typography, styled } from '@mui/material';
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
@@ -15,7 +15,24 @@ const OuterDiv = styled(Box)(({ theme }) => ({
   }));
   const InnerDiv = styled(Box)(({ theme }) => ({
     // border:'2px solid red',
+    display:"flex",
+    flexDirection:"column",
+    justifyContent:"center",
+    alignItems:"center",
 
+    [theme.breakpoints.down("xl")]: {},
+    [theme.breakpoints.down("lg")]: {},
+    [theme.breakpoints.down("md")]: {},
+    [theme.breakpoints.down("sm")]: {},
+    [theme.breakpoints.down("xs")]: {},
+  }));
+
+  const BottomDiv = styled(Box)(({ theme }) => ({
+    // border:'2px solid red',
+    paddingBottom:100,
+    paddingTop:50,
+  
+  
     [theme.breakpoints.down("xl")]: {},
     [theme.breakpoints.down("lg")]: {},
     [theme.breakpoints.down("md")]: {},
@@ -60,8 +77,8 @@ const OuterDiv = styled(Box)(({ theme }) => ({
     display:"flex",
     justifyContent:"right",
     // border:'2px solid red',
-    width:"90%",
-    gap:160,
+    width:"80%",
+    gap:100,
 
 
     [theme.breakpoints.down("xl")]: {},
@@ -72,6 +89,21 @@ const OuterDiv = styled(Box)(({ theme }) => ({
   }));
 
   const MapText = styled(Typography)(({ theme }) => ({
+
+
+    [theme.breakpoints.down("xl")]: {},
+    [theme.breakpoints.down("lg")]: {},
+    [theme.breakpoints.down("md")]: {
+    },
+    [theme.breakpoints.down("sm")]: {},
+    [theme.breakpoints.down("xs")]: {},
+  }));
+
+  const TotalDiv = styled(Box)(({ theme }) => ({
+
+    // border:"2px solid red",
+    display:"flex",
+    gap:100,
 
 
     [theme.breakpoints.down("xl")]: {},
@@ -96,12 +128,31 @@ const OuterDiv = styled(Box)(({ theme }) => ({
     [theme.breakpoints.down("xs")]: {},
   }));
 
+
+  const Wrapper = styled(Box)(({ theme }) => ({
+    // border:'2px solid yellow',
+    display:"flex",
+    width:"80%",
+
+    
+
+    [theme.breakpoints.down("xl")]: {},
+    [theme.breakpoints.down("lg")]: {},
+    [theme.breakpoints.down("md")]: {},
+    [theme.breakpoints.down("sm")]: {},
+    [theme.breakpoints.down("xs")]: {},
+  }));
+
 function Cart() {
     const dispatch=useDispatch()
     const navigate=useNavigate()
     const cartData=useSelector((store)=>store.data.getCartData)
 
     console.log("catData",cartData)
+
+    let total=cartData?.reduce((acc,item,index)=>{
+        return acc+item.price*item.quant
+    },0)
 
     useEffect(()=>{
        dispatch(getCart())
@@ -119,11 +170,21 @@ function Cart() {
 
 
 </TitleText>
+
+<Wrapper>
             <ItemBox>
                 {cartData.map((item)=>(
                     <MapData>
                     <ImageBox as={"img"} src={item.images[0]}/>
-                    <MapText sx={{textAlign:"left"}}>{item.name}</MapText>
+                    
+               </MapData>
+                ))}
+
+
+ {cartData.map((item)=>(
+                    <MapData>
+                  
+                    <MapText>{item.name}</MapText>
                     <MapText>{item.quant}</MapText>
                     <MapText>{item.price}</MapText>
                </MapData>
@@ -131,7 +192,19 @@ function Cart() {
 
                 
             </ItemBox>
+</Wrapper>
 
+
+<BottomDiv>
+
+<TotalDiv>
+<TextBox sx={{fontWeight:600}}>SUBTOTAL</TextBox>
+<TextBox>₹ {total}</TextBox>
+</TotalDiv>
+<TextBox sx={{fontSize:8,color:'grey'}}>Shipping, taxes, and discount codes calculated at checkout</TextBox>
+<Button sx={{color:"white",background:"black",width:300,borderRadius:0,":hover":{color:"white",background:"black"}}}>CHECK OUT</Button>
+
+</BottomDiv>
         </InnerDiv>
 
 
