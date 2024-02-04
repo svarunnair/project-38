d7e6820b724eb7687699e593566a800998499c5c
Varun Nair <varunsnair1997@gmail.com>
2024-02-04 23:51:32 +0530

    info page

--- src/Pages/Info.jsx
+++ src/Pages/Info.jsx
@@ -1,99 +1,96 @@
-import { CheckBox } from '@mui/icons-material';
 import { Box, OutlinedInput, Typography, styled } from '@mui/material';
 import React from 'react'
 
 
+const OuterContainer = styled(Box)(({ theme }) => ({
 
-const OuterDiv = styled(Box)(({ theme }) => ({
-    border:"2px solid black",
-    display:"flex",
 
-    [theme.breakpoints.down("xl")]: {},
-    [theme.breakpoints.down("lg")]: {},
-    [theme.breakpoints.down("md")]: {},
-    [theme.breakpoints.down("sm")]: {},
-    [theme.breakpoints.down("xs")]: {},
-  }));
+  [theme.breakpoints.down("xl")]: {},
+  [theme.breakpoints.down("lg")]: {},
+  [theme.breakpoints.down("md")]: {},
+  [theme.breakpoints.down("sm")]: {},
+  [theme.breakpoints.down("xs")]: {},
+}));
+const InnerContainer = styled(Box)(({ theme }) => ({
 
-  const InnerDiv = styled(Box)(({ theme }) => ({
-    border:"2px solid red",
-    width:"60%",
-    display:'flex',
-    flexDirection:'column',
+  border:"2px solid red",
+  width:"60%",
 
-    [theme.breakpoints.down("xl")]: {},
-    [theme.breakpoints.down("lg")]: {},
-    [theme.breakpoints.down("md")]: {},
-    [theme.breakpoints.down("sm")]: {},
-    [theme.breakpoints.down("xs")]: {},
-  }));
+  [theme.breakpoints.down("xl")]: {},
+  [theme.breakpoints.down("lg")]: {},
+  [theme.breakpoints.down("md")]: {},
+  [theme.breakpoints.down("sm")]: {},
+  [theme.breakpoints.down("xs")]: {},
+}));
 
-  const CartDiv = styled(Box)(({ theme }) => ({
-    border:"2px solid blue",
+const CartDiv = styled(Box)(({ theme }) => ({
 
-    [theme.breakpoints.down("xl")]: {},
-    [theme.breakpoints.down("lg")]: {},
-    [theme.breakpoints.down("md")]: {},
-    [theme.breakpoints.down("sm")]: {},
-    [theme.breakpoints.down("xs")]: {},
-  }));
+  border:"2px solid green",
 
-  const IconDiv = styled(Box)(({ theme }) => ({
-    // border:"2px solid blue",
-    width:"15%",
-    padding:70,
+  [theme.breakpoints.down("xl")]: {},
+  [theme.breakpoints.down("lg")]: {},
+  [theme.breakpoints.down("md")]: {},
+  [theme.breakpoints.down("sm")]: {},
+  [theme.breakpoints.down("xs")]: {},
+}));
 
-    [theme.breakpoints.down("xl")]: {},
-    [theme.breakpoints.down("lg")]: {},
-    [theme.breakpoints.down("md")]: {},
-    [theme.breakpoints.down("sm")]: {},
-    [theme.breakpoints.down("xs")]: {},
-  }));
+const InputDiv = styled(Box)(({ theme }) => ({
 
-  const TextBox = styled(Typography)(({ theme }) => ({
-    display:"flex",
- 
-    [theme.breakpoints.down("xl")]: {},
-    [theme.breakpoints.down("lg")]: {},
-    [theme.breakpoints.down("md")]: {},
-    [theme.breakpoints.down("sm")]: {},
-    [theme.breakpoints.down("xs")]: {},
-  }));
+  border:"2px solid yellow",
 
-  const InputDiv = styled(Typography)(({ theme }) => ({
-    // display:"flex",
-    border:"2px solid green",
-    display:"flex",
-    flexDirection:"column",
- 
-    [theme.breakpoints.down("xl")]: {},
-    [theme.breakpoints.down("lg")]: {},
-    [theme.breakpoints.down("md")]: {},
-    [theme.breakpoints.down("sm")]: {},
-    [theme.breakpoints.down("xs")]: {},
-  }));
+  [theme.breakpoints.down("xl")]: {},
+  [theme.breakpoints.down("lg")]: {},
+  [theme.breakpoints.down("md")]: {},
+  [theme.breakpoints.down("sm")]: {},
+  [theme.breakpoints.down("xs")]: {},
+}));
+const TextBox = styled(Typography)(({ theme }) => ({
+
+
+  [theme.breakpoints.down("xl")]: {},
+  [theme.breakpoints.down("lg")]: {},
+  [theme.breakpoints.down("md")]: {},
+  [theme.breakpoints.down("sm")]: {},
+  [theme.breakpoints.down("xs")]: {},
+}));
+const IconDiv = styled(Box)(({ theme }) => ({
+
+  width:"15%",
+  padding:60,
+  display:"flex",
+  [theme.breakpoints.down("xl")]: {},
+  [theme.breakpoints.down("lg")]: {},
+  [theme.breakpoints.down("md")]: {},
+  [theme.breakpoints.down("sm")]: {},
+  [theme.breakpoints.down("xs")]: {},
+}));
 
 function Info() {
   return (
-    <OuterDiv>
+    <OuterContainer>
 
-        <InnerDiv>
-            <IconDiv as={"img"} src="https://cdn.shopify.com/s/files/1/0696/1011/1257/files/Skybags_Logo._120x_5d2632d5-f10c-4723-9362-94e066a323c8_x320.png?v=1677825186"/>
-            
-            <InputDiv>
-            <TextBox>Contact</TextBox>
-            <OutlinedInput placeholder='Email'/>
-           <CheckBox/>Email me with news and offers
+      <InnerContainer>
 
+        <IconDiv as={"img"} src="https://cdn.shopify.com/s/files/1/0696/1011/1257/files/Skybags_Logo._120x_5d2632d5-f10c-4723-9362-94e066a323c8_x320.png?v=1677825186"/>
 
-            </InputDiv>
-        </InnerDiv>
+         <InputDiv>
+         <TextBox>Contact</TextBox>
+         <OutlinedInput placeholder='Email'/>
+         
+         </InputDiv>
 
 
-        <CartDiv>
 
-        </CartDiv>
-    </OuterDiv>
+      </InnerContainer>
+
+      <CartDiv>
+
+      </CartDiv>
+
+
+
+
+    </OuterContainer>
   )
 }
 
